47ecc0be10c7fcf8d457fad215d6260fa45c3d9d
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-11-21 18:17:25 +0000

    adding some styling

--- full-stack/public/includes/views/templates/header.php
+++ full-stack/public/includes/views/templates/header.php
@@ -2,26 +2,30 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <style type='text/css'>
-        .input-field label {
-            color: teal !important;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Graze</title>
 </head>
 
-<body class='teal lighten-4 container'>
-    <div class="row container input-field">
+<body class=''>
+    <nav>
+        <div class='nav-wrapper'>
+        <img src='static/assets/logo.svg' style='width:160px; height: auto; display: block; margin-bottom: 1rem;'>
 
-        <img src='static/assets/logo.svg' style='width:160px; height: auto; display: block; margin-bottom: 1rem;' class='col s6'>
+        <ul>
+            <li>Input</li>
+        </ul>
+        </div>
+
+    </nav>
+    <!-- <div class="row container nav-wrapper">
 
 
-        <div class='col s8'>
-            <form class='col s6 row'>
-                <label for='submit' class="col s8">Enter Account ID Below:</label>
+
+        <div class='row'>
+            <form class='input-field inline'>
+                <label for='submit' >Enter Account ID Below:</label>
                 <input type='text' name='submit' action='../boxes' method='GET' />
-                <button type='submit' class="waves-effect waves-light btn col">Submit</button>
+                <button type='submit' class="waves-effect waves-light btn" style='width:160px'>Submit</button>
             </form>
         </div>
-    </div>
+    </div> -->
